ec337262dd0293aa89e7f448fb8de53d37c7a9a6
cryptmick5 <cryptmick@gmail.com>
2026-07-11 21:30:26 +0200

    Zones injection interactives (schema SVG original, textes reecrits) - cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = "reta-sibo-v3";
+﻿const CACHE = "reta-sibo-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -22,5 +22,6 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
 
 
