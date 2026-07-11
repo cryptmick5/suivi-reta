e0fb3e71d4a066ef2fee5f54939d04031ea52acd
cryptmick5 <cryptmick@gmail.com>
2026-07-11 20:59:32 +0200

    Idees repas + fix courbe poids + audit UX (cache v3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = "reta-sibo-v2";
+﻿const CACHE = "reta-sibo-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -22,4 +22,5 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
 
